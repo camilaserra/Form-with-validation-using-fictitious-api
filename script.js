d04e334b29bcea408d0e7d2fec65ba7c8fdbc48a
Camila Serra <camilaserrasilva@gmail.com>
2021-09-01 19:40:44 -0300

    correção do erro de promisse

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const init = () => {
-    const inputEmail = document.querySelector('input[type="email"]');
-    const inputPassword = document.querySelector('input[type="password"]');
-    const submitButton = document.querySelector('.login__submit');
+const init = ( ) => {
+    const inputEmail = document.querySelector('input[type="email"]')
+    const inputPassword = document.querySelector('input[type="password"]')
+    const submitButton = document.querySelector('.login__submit')
 
-    if (submitButton) {
+    if(submitButton) {
         submitButton.addEventListener('click', (event) => {
             event.preventDefault();
 
             fetch('https://reqres.in/api/login', {
                 method: 'POST',
                 headers: {
-                    'ContentType': 'application/json'
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
                     email: inputEmail.value,
@@ -21,8 +21,11 @@ const init = () => {
             }).then((data) => {
                 console.log(data)
             })
+            
         })
     }
+   
+    // console.log(inputPassword, inputEmail, submitButton)
 }
 
 window.onload = init;
